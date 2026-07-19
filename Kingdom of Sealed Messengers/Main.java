a179b0bfa361a862180ef011cc27a9347ed328d5
MohmedQayf <md.kaif4all@gmail.com>
2026-07-19 10:03:42 +0000

    19th july done

--- Kingdom of Sealed Messengers/Main.java
+++ Kingdom of Sealed Messengers/Main.java
@@ -1,13 +1,35 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 class Main {
 
     public static void main(String[] args) {
        Scanner sc=new Scanner(System.in);
+       HashSet<Integer> set=new HashSet<>();
+       int N=sc.nextInt();
+       int result=0;
+       int M=sc.nextInt();
+       if(M==0){
+           System.out.println(N);
+           return;
+       }
+       int[][] arr=new int[M][2];
+       for (int i = 0; i < M; i++) {
+           for(int j=0;j<2;j++)
+           {
+            arr[i][j]=sc.nextInt();
+           }
+           
+            if(set.contains(arr[i][0])){
+                set.add(arr[i][1]);
+            }
+            else{
+                set.add(arr[i][0]);
+                set.add(arr[i][1]);
+                result++;
+            }
+           
+       }
+       System.out.println(result);
        sc.close();
     }
 }
